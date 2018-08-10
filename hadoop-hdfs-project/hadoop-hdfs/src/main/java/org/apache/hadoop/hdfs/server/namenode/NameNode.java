de470f71b40b3cb585980ac5e92793082c088ff7
Chao Sun <sunchao@apache.org>
2018-08-10 15:59:39 -0700

    Remove super user privilege requirement for NameNode.getServiceStatus. Contributed by Chao Sun.
    Summary:
    (cherry picked from commit a2a8c486998b81d2c73804a07cc74f5269bfd904)
    
    ObserverReadProxyProvider needs to call this API to find next observer, active name node, etc.
    
    Reviewers: aihuaxu, O2123 User sunchao: Add reviewers, O2125 User ekanth: Add reviewers, O2127 User pgolash: Add reviewers, O2121 Project Hadoop-HDFS Dev: Add blocking reviewers, mmathew
    
    Reviewed By: O2121 Project Hadoop-HDFS Dev: Add blocking reviewers, mmathew
    
    Subscribers: jeffreyz, O2133 Project Data Foundation - Hadoop Ecosystem: Add subscribers, O2131 Project Hadoop-HDFS Dev: Add subscribers
    
    Differential Revision: https://code.uberinternal.com/D5069385

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNode.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNode.java
@@ -1757,7 +1757,6 @@ public class NameNode implements NameNodeStatusMXBean {
 
   synchronized HAServiceStatus getServiceStatus()
       throws ServiceFailedException, AccessControlException {
-    namesystem.checkSuperuserPrivilege();
     if (!haEnabled) {
       throw new ServiceFailedException("HA for namenode is not enabled");
     }
