c1b19285a4e4053bcfddb2df1fdd646e67e4cd8a
Yiqun Lin <yqlin@apache.org>
2018-12-05 11:44:38 +0800

    HDFS-14114. RBF: MIN_ACTIVE_RATIO should be configurable. Contributed by Fei Hui.
    Summary:
    Expose the configuration of the minimum ratio of active connections to total connections.
    By that, we can reduce the idle connections and reduce connection pressure to NameNodes by increasing
    such value when there are connection spikes.
    
    Test Plan: unit test
    
    Reviewers: O2123 User sunchao: Add reviewers, O2125 User ekanth: Add reviewers, O2127 User pgolash: Add reviewers, O2121 Project Hadoop-HDFS Dev: Add blocking reviewers, lfengnan, qifan, jingzhao
    
    Reviewed By: O2121 Project Hadoop-HDFS Dev: Add blocking reviewers, lfengnan, qifan
    
    Subscribers: O2133 Project Data Foundation - Hadoop Ecosystem: Add subscribers, O2131 Project Hadoop-HDFS Dev: Add subscribers
    
    JIRA Issues: HDFS-1051
    
    Differential Revision: https://code.uberinternal.com/D5741555

--- hadoop-hdfs-project/hadoop-hdfs-rbf/src/main/java/org/apache/hadoop/hdfs/server/federation/router/ConnectionManager.java
+++ hadoop-hdfs-project/hadoop-hdfs-rbf/src/main/java/org/apache/hadoop/hdfs/server/federation/router/ConnectionManager.java
@@ -51,14 +51,7 @@ public class ConnectionManager {
   private static final Logger LOG =
           LoggerFactory.getLogger(ConnectionManager.class);
 
-  /**
-   * Minimum amount of active connections: 50%.
-   */
-  protected static final float MIN_ACTIVE_RATIO = 0.5f;
-
-  /**
-   * Configuration for the connection manager, pool and sockets.
-   */
+  /** Configuration for the connection manager, pool and sockets. */
   private final Configuration conf;
 
   /**
@@ -69,6 +62,8 @@ public class ConnectionManager {
    * Max number of connections per user + nn.
    */
   private final int maxSize;
+  /** Min ratio of active connections per user + nn. */
+  private final float minActiveRatio;
 
   /**
    * How often we close a pool for a particular user + nn.
@@ -127,10 +122,14 @@ public class ConnectionManager {
   public ConnectionManager(Configuration config, RouterRpcMonitor monitor) {
     this.conf = config;
     this.rpcMonitor = monitor;
-    // Configure minimum and maximum connection pools
+
+    // Configure minimum, maximum and active connection pools
     this.maxSize = this.conf.getInt(
-            RBFConfigKeys.DFS_ROUTER_NAMENODE_CONNECTION_POOL_SIZE,
-            RBFConfigKeys.DFS_ROUTER_NAMENODE_CONNECTION_POOL_SIZE_DEFAULT);
+        RBFConfigKeys.DFS_ROUTER_NAMENODE_CONNECTION_POOL_SIZE,
+        RBFConfigKeys.DFS_ROUTER_NAMENODE_CONNECTION_POOL_SIZE_DEFAULT);
+    this.minActiveRatio = this.conf.getFloat(
+        RBFConfigKeys.DFS_ROUTER_NAMENODE_CONNECTION_MIN_ACTIVE_RATIO,
+        RBFConfigKeys.DFS_ROUTER_NAMENODE_CONNECTION_MIN_ACTIVE_RATIO_DEFAULT);
 
     // Map with the connections indexed by UGI and Namenode
     this.pools = new HashMap<>();
@@ -230,7 +229,8 @@ public class ConnectionManager {
         pool = this.pools.get(connectionId);
         if (pool == null) {
           pool = new ConnectionPool(
-                  this.conf, nnAddress, ugi, this.minSize, this.maxSize);
+              this.conf, nnAddress, ugi, this.minSize, this.maxSize,
+              this.minActiveRatio);
           this.pools.put(connectionId, pool);
         }
       } finally {
@@ -390,11 +390,12 @@ public class ConnectionManager {
       long timeSinceLastActive = Time.now() - pool.getLastActiveTime();
       int total = pool.getNumConnections();
       int activeRecently = pool.getNumActiveConnectionsRecently();
+      float poolMinActiveRatio = pool.getMinActiveRatio();
       if (timeSinceLastActive > connectionCleanupPeriodMs ||
-          activeRecently < MIN_ACTIVE_RATIO * total) {
+          activeRecently < poolMinActiveRatio * total) {
         // Be greedy here to close as many connections as possible in one shot
         int targetConnectionsCount =
-            (int)(MIN_ACTIVE_RATIO * total) - activeRecently;
+            (int)(poolMinActiveRatio * total) - activeRecently;
         List<ConnectionContext> conns =
             pool.removeConnections(targetConnectionsCount);
         for (ConnectionContext conn : conns) {
@@ -491,8 +492,9 @@ public class ConnectionManager {
           try {
             int total = pool.getNumConnections();
             int active = pool.getNumActiveConnectionsRecently();
+            float poolMinActiveRatio = pool.getMinActiveRatio();
             if (pool.getNumConnections() < pool.getMaxSize() &&
-                    active >= MIN_ACTIVE_RATIO * total) {
+                active >= poolMinActiveRatio * total) {
               long startTime = Time.now();
               ConnectionContext conn = pool.newConnection();
               pool.addConnection(conn);
