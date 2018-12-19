0daf1f3374327b739941413f1a2c929337f4339d
Konstantin V Shvachko <shv@apache.org>
2018-12-19 12:39:57 -0800

    HDFS-14160. [SBN read] ObserverReadInvocationHandler should implement RpcInvocationHandler. Contributed by Konstantin V Shvachko.

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/ha/TestObserverNode.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/ha/TestObserverNode.java
@@ -52,6 +52,7 @@ import org.apache.hadoop.hdfs.server.blockmanagement.BlockInfo;
 import org.apache.hadoop.hdfs.server.blockmanagement.BlockInfoContiguous;
 import org.apache.hadoop.hdfs.server.blockmanagement.BlockManager;
 import org.apache.hadoop.hdfs.server.namenode.NameNodeAdapter;
+import org.apache.hadoop.hdfs.server.namenode.TestFsck;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -358,6 +359,18 @@ public class TestObserverNode {
     assertSentTo(0);
 
     Mockito.reset(bmSpy);
+  }
+
+  @Test
+  public void testFsckWithObserver() throws Exception {
+    setObserverRead(true);
+
+    dfs.create(testPath, (short)1).close();
+    assertSentTo(0);
+
+    final String result = TestFsck.runFsck(conf, 0, true, "/");
+    LOG.info("result=" + result);
+    assertTrue(result.contains("Status: HEALTHY"));
   }
 
   private void assertSentTo(int nnIdx) throws IOException {
