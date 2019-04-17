c0bfa73ac220cda283dc8a5c75f67b6b3e83436a
Erik Krogen <xkrogen@apache.org>
2019-04-17 14:38:24 -0700

    HDFS-14245. [SBN read] Enable ObserverReadProxyProvider to work with non-ClientProtocol proxy types. Contributed by Erik Krogen.
    (cherry picked from 5847e0014343f60f853cb796781ca1fa03a72efd)
    (cherry picked from 6630c9b75d65deefb5550e355eef7783909a57bc)
    (cherry picked from 9fdb849e034573bb44abd593eefa1e13a3261376)

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/ha/TestObserverNode.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/ha/TestObserverNode.java
@@ -54,6 +54,7 @@ import org.apache.hadoop.hdfs.server.blockmanagement.BlockInfoContiguous;
 import org.apache.hadoop.hdfs.server.blockmanagement.BlockManager;
 import org.apache.hadoop.hdfs.server.namenode.NameNodeAdapter;
 import org.apache.hadoop.hdfs.server.namenode.TestFsck;
+import org.apache.hadoop.hdfs.tools.GetGroups;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -121,6 +122,17 @@ public class TestObserverNode {
       return;
     }
     fail("active cannot be transitioned to observer");
+  }
+
+  /**
+   * Test that non-ClientProtocol proxies such as
+   * {@link org.apache.hadoop.tools.GetUserMappingsProtocol} still work
+   * when run in an environment with observers.
+   */
+  @Test
+  public void testGetGroups() throws Exception {
+    GetGroups getGroups = new GetGroups(conf);
+    assertEquals(0, getGroups.run(new String[0]));
   }
 
   @Test
