dc0669c787a628ec8895ac6c68749655e6f2fd2e
Qifan Shi <qifan@uber.com>
2020-04-23 15:24:15 -0700

    Add secure pakcage and krb5.conf in HDFS image.
    Summary: Add secure pakcage and krb5.conf in HDFS image.
    
    Reviewers: O2123 User sunchao: Add reviewers, O2125 User ekanth: Add reviewers, O2127 User pgolash: Add reviewers, O2121 Project Hadoop-HDFS Dev: Add blocking reviewers, mmathew, zhangf, #data_automation, liangg, sunchao
    
    Reviewed By: O2123 User sunchao: Add reviewers, O2121 Project Hadoop-HDFS Dev: Add blocking reviewers, mmathew, #data_automation, sunchao
    
    Subscribers: mingmin, lkrishna, O2131 Project Hadoop-HDFS Dev: Add subscribers, chloek, O2133 Project Data Foundation - Hadoop Ecosystem: Add subscribers
    
    Maniphest Tasks: T5804251
    
    Differential Revision: https://code.uberinternal.com/D4290563

--- udeploy/hdfs/config.sh
+++ udeploy/hdfs/config.sh
@@ -5,59 +5,100 @@ set -o pipefail
 
 mkdir -p ${HDFS_CONF_PATH}
 
+
 # Copy over dsc configs to target path.
-bash ${DSC_SETUP_SCRIPT} ${DSC_TARGET_DIR}
-rsync --exclude *.sha -av ${DSC_TARGET_DIR}/${DSC_SUB_PATH}/* ${HDFS_CONF_PATH}/
-cp ./udeploy/hdfs/topology.py ${HDFS_CONF_PATH}/
-cp -r ./udeploy/hdfs/jsvc ${HDFS_CONF_PATH}/jsvc
+config_hdfs_conf() {
+  bash ${DSC_SETUP_SCRIPT} ${DSC_TARGET_DIR}
+  rsync --exclude *.sha -av ${DSC_TARGET_DIR}/${DSC_SUB_PATH}/* ${HDFS_CONF_PATH}/
+  cp ./udeploy/hdfs/topology.py ${HDFS_CONF_PATH}/
+  cp -r ./udeploy/hdfs/jsvc ${HDFS_CONF_PATH}/jsvc
+}
 
-# Update ssl-server.xml and ssl-client.xml with passwords
-if [ -f ${TRUSTSTORE_PSD} ] ; then
-  truststore_passwd=$(sudo cat ${TRUSTSTORE_PSD})
-  [ -f ${SSL_CLIENT} ] && sed -i "s|PLACE_HOLDER_FOR_TRUSTSTORE_PASSWORD|${truststore_passwd}|" ${SSL_CLIENT}
-  [ -f ${SSL_SERVER} ] && sed -i "s|PLACE_HOLDER_FOR_TRUSTSTORE_PASSWORD|${truststore_passwd}|" ${SSL_SERVER}
-fi
-if [ -f ${KEYSTORE_PSD} ] ; then
-  keystore_passwd=$(sudo cat ${KEYSTORE_PSD})
-  [ -f ${SSL_SERVER} ] && sed -i "s|PLACE_HOLDER_FOR_KEYSTORE_PASSWORD|${keystore_passwd}|" ${SSL_SERVER}
-fi
+# Populate ssl secrets, setup principal and keytabs.
+config_secrets() {
+  # Update ssl-server.xml and ssl-client.xml with passwords
+  if [ -f ${TRUSTSTORE_PSD} ] ; then
+    truststore_passwd=$(sudo cat ${TRUSTSTORE_PSD})
+    [ -f ${SSL_CLIENT} ] && sed -i "s|PLACE_HOLDER_FOR_TRUSTSTORE_PASSWORD|${truststore_passwd}|" ${SSL_CLIENT}
+    [ -f ${SSL_SERVER} ] && sed -i "s|PLACE_HOLDER_FOR_TRUSTSTORE_PASSWORD|${truststore_passwd}|" ${SSL_SERVER}
+  fi
+  if [ -f ${KEYSTORE_PSD} ] ; then
+    keystore_passwd=$(sudo cat ${KEYSTORE_PSD})
+    [ -f ${SSL_SERVER} ] && sed -i "s|PLACE_HOLDER_FOR_KEYSTORE_PASSWORD|${keystore_passwd}|" ${SSL_SERVER}
+  fi
 
-if [ ! -d ${SECURE_STORE_PATH} ]; then
-  mkdir -p "${SECURE_STORE_PATH}"
-fi
+  if [ ! -d ${SECURE_STORE_PATH} ]; then
+    mkdir -p "${SECURE_STORE_PATH}"
+  fi
 
-if [ -f ${TRUSTSTORE_FILE} ] ; then
-    sudo cp ${TRUSTSTORE_FILE} ${SECURE_STORE_PATH}/truststore.jks
-    sudo chmod 444 ${SECURE_STORE_PATH}/truststore.jks
-fi
-if [ -f ${KEYSTORE_FILE} ] ; then
-    sudo cp ${KEYSTORE_FILE} ${SECURE_STORE_PATH}/server.keystore
-    sudo chmod 440 ${SECURE_STORE_PATH}/server.keystore
-fi
+  if [ -f ${TRUSTSTORE_FILE} ] ; then
+      sudo cp ${TRUSTSTORE_FILE} ${SECURE_STORE_PATH}/truststore.jks
+      sudo chmod 444 ${SECURE_STORE_PATH}/truststore.jks
+  fi
+  if [ -f ${KEYSTORE_FILE} ] ; then
+      sudo cp ${KEYSTORE_FILE} ${SECURE_STORE_PATH}/server.keystore
+      sudo chmod 440 ${SECURE_STORE_PATH}/server.keystore
+  fi
 
-# fetch keytab and langley secrets from files landed by odin-hdfs-worker
-if [ -f ${ODIN_TRUSTSTORE_PSD} ] && [ -s ${ODIN_TRUSTSTORE_PSD} ]; then
-  truststore_passwd=$(sudo cat ${ODIN_TRUSTSTORE_PSD})
-  [ -f ${SSL_CLIENT} ] && sed -i "s|PLACE_HOLDER_FOR_TRUSTSTORE_PASSWORD|${truststore_passwd}|" ${SSL_CLIENT}
-  [ -f ${SSL_SERVER} ] && sed -i "s|PLACE_HOLDER_FOR_TRUSTSTORE_PASSWORD|${truststore_passwd}|" ${SSL_SERVER}
-fi
-if [ -f ${ODIN_KEYSTORE_PSD} ] && [ -s ${ODIN_KEYSTORE_PSD} ]; then
-  keystore_passwd=$(sudo cat ${ODIN_KEYSTORE_PSD})
-  [ -f ${SSL_SERVER} ] && sed -i "s|PLACE_HOLDER_FOR_KEYSTORE_PASSWORD|${keystore_passwd}|" ${SSL_SERVER}
-fi
+  # fetch langley secrets from files landed by odin-hdfs-worker
+  if [ -f ${ODIN_TRUSTSTORE_PSD} ] && [ -s ${ODIN_TRUSTSTORE_PSD} ]; then
+    truststore_passwd=$(sudo cat ${ODIN_TRUSTSTORE_PSD})
+    [ -f ${SSL_CLIENT} ] && sed -i "s|PLACE_HOLDER_FOR_TRUSTSTORE_PASSWORD|${truststore_passwd}|" ${SSL_CLIENT}
+    [ -f ${SSL_SERVER} ] && sed -i "s|PLACE_HOLDER_FOR_TRUSTSTORE_PASSWORD|${truststore_passwd}|" ${SSL_SERVER}
+  fi
+  if [ -f ${ODIN_KEYSTORE_PSD} ] && [ -s ${ODIN_KEYSTORE_PSD} ]; then
+    keystore_passwd=$(sudo cat ${ODIN_KEYSTORE_PSD})
+    [ -f ${SSL_SERVER} ] && sed -i "s|PLACE_HOLDER_FOR_KEYSTORE_PASSWORD|${keystore_passwd}|" ${SSL_SERVER}
+  fi
 
-if [ -f ${ODIN_TRUSTSTORE_FILE} ] && [ -s ${ODIN_TRUSTSTORE_FILE} ]; then
-    sudo cp ${ODIN_TRUSTSTORE_FILE} ${SECURE_STORE_PATH}/truststore.jks
-    sudo chmod 444 ${SECURE_STORE_PATH}/truststore.jks
-fi
-if [ -f ${ODIN_KEYSTORE_FILE} ] && [ -s ${ODIN_KEYSTORE_FILE} ]; then
-    sudo cp ${ODIN_KEYSTORE_FILE} ${SECURE_STORE_PATH}/server.keystore
-    sudo chmod 440 ${SECURE_STORE_PATH}/server.keystore
-fi 
+  if [ -f ${ODIN_TRUSTSTORE_FILE} ] && [ -s ${ODIN_TRUSTSTORE_FILE} ]; then
+      sudo cp ${ODIN_TRUSTSTORE_FILE} ${SECURE_STORE_PATH}/truststore.jks
+      sudo chmod 444 ${SECURE_STORE_PATH}/truststore.jks
+  fi
+  if [ -f ${ODIN_KEYSTORE_FILE} ] && [ -s ${ODIN_KEYSTORE_FILE} ]; then
+      sudo cp ${ODIN_KEYSTORE_FILE} ${SECURE_STORE_PATH}/server.keystore
+      sudo chmod 440 ${SECURE_STORE_PATH}/server.keystore
+  fi 
 
-# update hostname placeholder in principal value in core/hdfs-site.xml, use system HOSTNAME env.
-[ -f ${CORE_SITE} ] && sed -i "s|HOSTNAME|${HOSTNAME}|" ${CORE_SITE}
-[ -f ${HDFS_SITE} ] && sed -i "s|HOSTNAME|${HOSTNAME}|" ${HDFS_SITE}
+  # update hostname placeholder in principal value in core/hdfs-site.xml, use system HOSTNAME env.
+  [ -f ${CORE_SITE} ] && sed -i "s|HOSTNAME|${HOSTNAME}|" ${CORE_SITE}
+  [ -f ${HDFS_SITE} ] && sed -i "s|HOSTNAME|${HOSTNAME}|" ${HDFS_SITE}
 
-# set permissions for secrets
-sudo chown -R udocker:udocker ${SECURE_STORE_PATH}
+  # set permissions for secrets
+  sudo chown -R udocker:udocker ${SECURE_STORE_PATH}
+}
+
+setup_production_kdc_env() {
+    if [[ -z $UBER_REGION ]]; then
+        echo 'ERROR: Env UBER_REGION not set' && exit 1
+    fi
+
+    export KRB_KDC_REALM='DATASRE.PROD.UBER.INTERNAL'
+    export KRB_KDC_HOST_ADDR="hadoopkdc-${UBER_REGION}.uber.internal"
+    export KRB_ADMIN_SERVER_HOST_ADDR='hadoopldap00-phx2.prod.uber.internal'
+    export KRB_DOMAIN_REALM='prod.uber.internal'
+}
+
+setup_staging_kdc_env() {
+    export KRB_KDC_REALM='DATA.STAGING.UBER.INTERNAL'
+    export KRB_KDC_HOST_ADDR="hadoopkdcstaging-phx.uber.internal"
+    export KRB_ADMIN_SERVER_HOST_ADDR='hadoopkdcstaging01-phx2.prod.uber.internal'
+    export KRB_DOMAIN_REALM='prod.uber.internal'
+}
+
+config_krb5_conf() {
+  if [[ $UBER_RUNTIME_ENVIRONMENT == 'production' ]]; then
+      setup_production_kdc_env
+  else
+      [[ $UBER_RUNTIME_ENVIRONMENT == 'staging' ]]
+      setup_staging_kdc_env
+  fi
+  
+  sudo rm -f "${KRB5_CONF_PATH}"
+  sudo install -o udocker -g udocker -m 644 /dev/null /etc/krb5.conf
+  envsubst < "${KRB5_CONF_TEMPLATE}" > "${KRB5_CONF_PATH}"
+}
+
+config_hdfs_conf
+config_secrets
+config_krb5_conf
