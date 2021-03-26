b2c913aa8496aca7f558a7327bf35d8be87728f9
Devesh Agrawal <dagrawal@uber.com>
2021-03-25 18:24:13 -0700

    [HDP] Allow passing in the NEW_VERSION using the jenkins RELEASE_VERSION parameter
    Summary: Without this, the script is calculating the version number as 164. But unfortunately, 164 is already pushed and thus cannot be replaced. We furthermore don't support SNAPSHOT jars. This is an attempt to work around this by publishing a user specified version.
    
    Test Plan: Successfully built a test jar using https://ci.uberinternal.com/job/hadoop-common-release/113/console with my name on it.
    
    Reviewers: jingzhao, lfengnan, mmathew, aihuaxu, O2123 User sunchao: Add reviewers, O2125 User ekanth: Add reviewers, O2127 User pgolash: Add reviewers, O2121 Project Hadoop-HDFS Dev: Add blocking reviewers
    
    Reviewed By: lfengnan, O2121 Project Hadoop-HDFS Dev: Add blocking reviewers
    
    Subscribers: jeffreyz, O2133 Project Data Foundation - Hadoop Ecosystem: Add subscribers, O2131 Project Hadoop-HDFS Dev: Add subscribers
    
    JIRA Issues: HDP-18259
    
    Differential Revision: https://code.uberinternal.com/D5858423

--- uber-update-version.sh
+++ uber-update-version.sh
@@ -1,18 +1,17 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 BASE_VERSION=2.8.2
-START_COMMIT=042c04d
 
-echo "Calculating the new version number ..."
-REVISION=$(git log --oneline | grep $START_COMMIT -n | cut -d ':' -f 1)
-NEW_VERSION=${BASE_VERSION}-uber-${REVISION}
+if [[ -z "$RELEASE_VERSION" ]]; then
+    NEW_VERSION="uber-hadoop-$BASE_VERSION-$(git rev-parse --short=16 HEAD)"
+else
+    NEW_VERSION="uber-hadoop-$BASE_VERSION-$RELEASE_VERSION"
+fi
 
 echo "Setting new version to be ${NEW_VERSION}"
 cd "$(dirname "$0")"
 mvn versions:set -DnewVersion=${NEW_VERSION}
 sed -i.bak "s/VERSION=.*/VERSION=${NEW_VERSION}/" uber-mvn-deploy.sh
-rm uber-mvn-deploy.sh.bak
-
 echo "Cleaning up ..."
-find . -name '*.versionsBackup' -exec rm {} \;
+mvn versions:commit
 echo "Done"
